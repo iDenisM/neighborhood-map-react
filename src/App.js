717385b5a23824fa3602f0f1732d1daa97bcdd87
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2018-03-21 00:06:59 +0100

    Add large info window for markers

--- src/App.js
+++ src/App.js
@@ -3,21 +3,54 @@ import './App.css';
 
 export default class App extends Component {
   state = {
-    points: [{
-      lat: 40.719526, lng: -74.0089934
-    }]
+    map: {
+      center: {lat: 44.592301, lng: 11.051069},
+      zoom: 16,
+      mapTypeId: 'roadmap',
+    },
+    locations: [{
+      title: 'My Home',
+      position: {lat: 44.592301, lng: 11.051069}
+    },
+    {
+      title: 'Coop Super Market',
+      position: {lat: 44.593414, lng: 11.050536}
+    }],
+    markers: []
   }
 
   componentDidMount() {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 44.592301, lng: 11.051069},
-      zoom: 13,
-      mapTypeId: 'roadmap',
+    let map = new window.google.maps.Map(document.getElementById('map'), this.state.map),
+        largeInfoWindow = new window.google.maps.InfoWindow()
+
+    this.state.locations.map((location, index) => {
+      // Create the location
+      let newMarker = new window.google.maps.Marker({
+        map: map,
+        position: location.position,
+        title: location.title,
+        animation: window.google.maps.Animation.DROP,
+        id: index
+      })
+      // Push the new location to the markers holder
+      this.setState((prevState) => {
+        markers: [...prevState.markers, newMarker]
+      })
+      //Event lisners
+      newMarker.addListener('click', () => this.populateInfoWindow(map, newMarker, largeInfoWindow))
+      return location
     })
-    let marker = new window.google.maps.Marker({
-      map: map,
-      position: {lat: 44.592301, lng: 11.051069}
-    })
+  }
+
+  populateInfoWindow = (map, marker, infoWindow) => {
+    // Check if on this maker the info window isn't opened
+    if (infoWindow.marker !== marker) {
+      infoWindow.marker = marker
+      infoWindow.setContent(`<div>${marker.title}</div>`)
+      infoWindow.open(map, marker)
+      // Clear the marker if window is closed
+      infoWindow.addListener('closeClick', () => infoWindow.setMarker(null))
+    }
   }
 
   render() {

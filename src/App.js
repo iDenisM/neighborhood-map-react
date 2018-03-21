96073012e22fb24c86e1d59f914058c9b3569c02
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2018-03-21 16:28:58 +0100

    Create Map class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from './Map'
 import './App.css';
 
 export default class App extends Component {
@@ -6,7 +7,7 @@ export default class App extends Component {
     map: {
       center: {lat: 44.592301, lng: 11.051069},
       zoom: 16,
-      mapTypeId: 'roadmap',
+      mapTypeId: 'roadmap'
     },
     locations: [{
       title: 'My Home',
@@ -15,42 +16,7 @@ export default class App extends Component {
     {
       title: 'Coop Super Market',
       position: {lat: 44.593414, lng: 11.050536}
-    }],
-    markers: []
-  }
-
-  componentDidMount() {
-    let map = new window.google.maps.Map(document.getElementById('map'), this.state.map),
-        largeInfoWindow = new window.google.maps.InfoWindow()
-
-    this.state.locations.map((location, index) => {
-      // Create the location
-      let newMarker = new window.google.maps.Marker({
-        map: map,
-        position: location.position,
-        title: location.title,
-        animation: window.google.maps.Animation.DROP,
-        id: index
-      })
-      // Push the new location to the markers holder
-      this.setState((prevState) => {
-        markers: [...prevState.markers, newMarker]
-      })
-      //Event lisners
-      newMarker.addListener('click', () => this.populateInfoWindow(map, newMarker, largeInfoWindow))
-      return location
-    })
-  }
-
-  populateInfoWindow = (map, marker, infoWindow) => {
-    // Check if on this maker the info window isn't opened
-    if (infoWindow.marker !== marker) {
-      infoWindow.marker = marker
-      infoWindow.setContent(`<div>${marker.title}</div>`)
-      infoWindow.open(map, marker)
-      // Clear the marker if window is closed
-      infoWindow.addListener('closeClick', () => infoWindow.setMarker(null))
-    }
+    }]
   }
 
   render() {
@@ -70,13 +36,16 @@ export default class App extends Component {
         </header>
         {/*The main part of the app structure*/}
         <main className='main'>
-          <div id='map'></div>
+          <Map
+            map={this.state.map}
+            locations={this.state.locations}
+          />
         </main>
         {/*The footer part of the app structure*/}
         <footer className='footer'>
 
         </footer>
       </div>
-    );
+    )
   }
 }
---
+++ src/Map.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+export default class Map extends Component {
+  state = {
+    markers: []
+  }
+
+  componentDidMount() {
+    let map = new window.google.maps.Map(document.getElementById('map'), this.props.map),
+        largeInfoWindow = new window.google.maps.InfoWindow()
+
+    this.props.locations.map((location, index) => {
+      // Create the location
+      let newMarker = new window.google.maps.Marker({
+        map: map,
+        position: location.position,
+        title: location.title,
+        animation: window.google.maps.Animation.DROP,
+        id: index
+      })
+      // Push the new location to the markers holder
+      this.setState((prevState) => {
+        markers: [...prevState.markers, newMarker]
+      })
+      //Event lisners
+      newMarker.addListener('click', () => this.populateInfoWindow(map, newMarker, largeInfoWindow))
+      return location
+    })
+  }
+
+  populateInfoWindow = (map, marker, infoWindow) => {
+    // Check if on this maker the info window isn't opened
+    if (infoWindow.marker !== marker) {
+      infoWindow.marker = marker
+      infoWindow.setContent(`<div>${marker.title}</div>`)
+      infoWindow.open(map, marker)
+      // Clear the marker if window is closed
+      infoWindow.addListener('closeClick', () => infoWindow.setMarker(null))
+    }
+  }
+
+  render() {
+    return (
+      <div id='map'></div>
+    )
+  }
+}
